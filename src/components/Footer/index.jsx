d73397f58ecd01aace5948912be5022466458025
Luana Nicácio <70960344+LuanaNicacios@users.noreply.github.com>
2024-05-29 19:25:56 +0000

    Pending changes exported from your codespace

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,6 +4,7 @@ import Logo from "../../assets/Logo Branca.png";
 function Footer() {
   return (
     <footer>
+
       <div className="footer">
         <picture className="logo-footer">
           <img src={Logo} alt="Logo Viverde Branca" />
